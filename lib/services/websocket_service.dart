303057c7be030319889dd3179156412f1b72f51d
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-04 20:48:38 -0300

    :sparkles: feat: websocket communication class

---
+++ lib/services/websocket_service.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:stomp_dart_client/stomp_dart_client.dart';
+
+class WebsocketService {
+
+  late StompClient _stompClient;
+  String deliveryId = '';
+  bool isConnected = false;
+
+  void connect(String deliveryIdParam) async{
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token') ?? '';
+    
+    deliveryId = deliveryIdParam;
+  
+    _stompClient = StompClient(
+      config: StompConfig(
+        url: 'ws://192.168.1.102:8080/ws',
+        onConnect: _onConnect,
+        beforeConnect: () async {
+          await Future.delayed(const Duration(microseconds: 200));
+        },
+        onWebSocketError: (error) => debugPrint('Error: $error'),
+        stompConnectHeaders: {'Authorization' : 'Bearer $token'},
+        webSocketConnectHeaders: {'Authorization' : 'Bearer $token'}
+      )
+    );
+
+    _stompClient.activate();
+  }
+
+  void _onConnect(StompFrame frame){
+    isConnected = true;
+
+    _stompClient.subscribe(
+      destination: '/topic/location/$deliveryId',
+      callback: (frame) => debugPrint('Recebido do servidor: $frame')
+    );
+  }
+
+  void sendLocation(double lat, double lng){
+    if(!isConnected) return;
+
+    final body = jsonEncode({
+      'deliveryId': deliveryId,
+      'lat': lat,
+      'lng': lng
+    });
+
+    _stompClient.send(
+      destination: '/app/location',
+      body: body,
+    );
+  }
+
+  void disconnect(){
+    _stompClient.deactivate();
+    isConnected = false;
+  }
+
+}
